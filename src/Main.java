73ecbf8e130769bfd2ac3d8f0a620a1585273ff8
kegLeon <shakurov-40@mail.ru>
2025-08-24 20:02:34 +0400

    "поправил" оформление. Исправил решение.

--- src/Main.java
+++ src/Main.java
@@ -2,59 +2,59 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    //первая задача
-        var dog=8.0;
-            var cat=3.6;
-                var paper=763789;
+        //первая задача
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
         System.out.println("задача один, собака" + dog);
-            System.out.println("задача один, кот"+cat);
-                System.out.println("задача один, Бумага"+paper);
-    //вторая задача
-            dog=dog+4;
-                cat=cat+4;
-                    paper=paper+4;
-            System.out.println("задача 2 собака"+dog);
-                System.out.println("задача 2 кот"+cat);
-                    System.out.println("задача 2 бумага"+paper);
-    //третья задача
-            dog=dog-3.5;
-                cat=cat-1.6;
-                    paper=paper-7639;
-            System.out.println("третья задача собака"+dog);
-                 System.out.println("третья задача кот"+cat);
-                    System.out.println("третья задача бумага"+paper);
-    //четвертая задача
-        var friend=19;
-            System.out.println("задача 4 начало"+friend);
-        friend=friend+2;
-            System.out.println("задача 4 сложение"+friend);
-        friend=friend/7;
-            System.out.println("задача 4 деление"+friend);
-    //пятая задача
-        var frog=3.5;
-        System.out.println("задача 5, начало"+frog);
-            frog=frog*10;
-            System.out.println("задача 5 умножение"+frog);
-        frog=frog/3.5;
-        System.out.println("задача 5 деление"+frog);
-            frog=frog+4;
-            System.out.println("задача 5 сложение"+frog);
-    //шестая задача
-        var boxer1=78.2;
-        var boxer2=82.7;
-        System.out.println("сумма боксеров"+boxer1+boxer2);
-    //седьмая задача
-        var divBox=boxer2%boxer1;
-        System.out.println("остаток деления"+divBox);
-    //восьмая задача
-         var hours=640;
-         var peopleHours=8;
-         var numberPeople=hours/peopleHours;
-         System.out.println("Всего работников в компании —"+numberPeople+"человек");
-         var anotherComp=numberPeople+94;
-         var workHoursAnotherComp=anotherComp*8;
-         System.out.println("Если в компании работает"+anotherComp+ "человек, то всего"+workHoursAnotherComp+"часов " +
-                 "работы может быть поделено между сотрудниками");
+        System.out.println("задача один, кот" + cat);
+        System.out.println("задача один, Бумага" + paper);
+        //вторая задача
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("задача 2 собака" + dog);
+        System.out.println("задача 2 кот" + cat);
+        System.out.println("задача 2 бумага" + paper);
+        //третья задача
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("третья задача собака" + dog);
+        System.out.println("третья задача кот" + cat);
+        System.out.println("третья задача бумага" + paper);
+        //четвертая задача
+        var friend = 19;
+        System.out.println("задача 4 начало" + friend);
+        friend = friend + 2;
+        System.out.println("задача 4 сложение" + friend);
+        friend = friend / 7;
+        System.out.println("задача 4 деление" + friend);
+        //пятая задача
+        var frog = 3.5;
+        System.out.println("задача 5, начало" + frog);
+        frog = frog * 10;
+        System.out.println("задача 5 умножение" + frog);
+        frog = frog / 3.5;
+        System.out.println("задача 5 деление" + frog);
+        frog = frog + 4;
+        System.out.println("задача 5 сложение" + frog);
+        //шестая задача
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println("сумма боксеров" + boxer1 + boxer2);
+        //седьмая задача
+        var divBox = boxer2 % boxer1;
+        System.out.println("остаток деления" + divBox);
+        //восьмая задача
+        var hours = 640;
+        var peopleHours = 8;
+        var numberPeople = hours / peopleHours;
+        System.out.println("Всего работников в компании —" + numberPeople + "человек");
+        var anotherComp = numberPeople + 94;
+        var workHoursAnotherComp = 640/anotherComp;
+        System.out.println("Если в компании работает" + anotherComp + "человек, то всего" + workHoursAnotherComp + "часов " +
+                "работы может быть поделено между сотрудниками");
 
 
     }
